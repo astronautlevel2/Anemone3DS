f8fe7e7874d7895afe6285fd6c01f099cce9fd22
Dawid 'daedreth' Eckert <eckertdawid@gmail.com>
2017-07-12 12:57:40 +0200

    why is there errors

--- source/main.c
+++ source/main.c
@@ -8,54 +8,69 @@ u8 themeInstall()
 	u8 region;
 	u32 archive;
 	u32 archive2;
-	CFGU_SecureInfoGetRegion(&region);
-	if(region == 1){
-	archive = 0x000002cd;
-	archive2 = 0x0000008f;
-	}else if(region == 2){
-	archive = 0x000002ce;
-	archive2 = 0x00000098;
-	}else if(region == 3){
-	archive = 0x000002cc;
-	archive2 = 0x00000082;
-	}
-
 	char* body;
 	u64 bodySize;
 	char* bgm;
 	u64 bgmSize;
 
+	u32 bytes;
+
 	Handle bodyHandle;
 	Handle bgmHandle;
+
+	CFGU_SecureInfoGetRegion(&region);
+	switch(region)
+	{
+		case 1:
+			archive = 0x000002cd;
+			archive2 = 0x0000008f;		
+			break;
+		case 2:
+			archive = 0x000002ce;
+			archive2 = 0x00000090;
+			break;
+		case 3:
+			archive = 0x000002cc;
+			archive2 = 0x00000082;
+			break;
+		default:
+			archive = 0;
+			archive2 = 0;
+			break;
+	}
+	
 
 	FSUSER_OpenFile(&bodyHandle, ARCHIVE_SDMC, fsMakePath(PATH_ASCII, "/Themes/theme/body_LZ.bin"), FS_OPEN_READ, 0);
 	
 	FSFILE_GetSize(bodyHandle, &bodySize);
-	body = malloc((sizeof(char) * bodySize) + 8); // just in case
-	FSFILE_Read(bodyHandle, 0, 0, body, (u32)bodySize);
+	body = malloc(sizeof(char) * bodySize); 
+	FSFILE_Read(bodyHandle, &bytes, 0, body, (u32)bodySize);
 
 	FSUSER_OpenFile(&bgmHandle, ARCHIVE_SDMC, fsMakePath(PATH_ASCII, "/Themes/theme/bgm.bcstm"), FS_OPEN_READ, 0);
 	FSFILE_GetSize(bgmHandle, &bgmSize);
-	bgm = malloc((sizeof(char) * bgmSize) + 8); // just in case
-	FSFILE_Read(bgmHandle, 0, 0, bgm, (u32)bgmSize);
+	bgm = malloc(sizeof(char) * bgmSize);
+	FSFILE_Read(bgmHandle, &bytes, 0, bgm, (u32)bgmSize);
 
 	Handle saveDataHandle;
 	u8* saveDataBuffer;
 	u64 saveDataSize;
 
-	FSUSER_OpenFile(&saveDataHandle, archive2, fsMakePath(PATH_ASCII, "/SaveData.dat"), FS_OPEN_READ | FS_OPEN_WRITE, 0);
+	Result ret;
+	ret = FSUSER_OpenFile(&saveDataHandle, archive2, fsMakePath(PATH_ASCII, "/SaveData.dat"), FS_OPEN_READ | FS_OPEN_WRITE, 0);
+	return (u8)ret;
+	
 	FSFILE_GetSize(saveDataHandle, &saveDataSize);
 
 	saveDataBuffer = malloc(sizeof(u8) * saveDataSize);
-	FSFILE_Read(saveDataHandle, 0, 0, saveDataBuffer, (u32)saveDataSize);
+	FSFILE_Read(saveDataHandle, &bytes, 0, saveDataBuffer, (u32)saveDataSize);
 
-	memset(&saveDataBuffer[0x13b8], 0, 8); // I don't know what this does tbqh, taken straight from the docs
+	memset(&saveDataBuffer[0x13b8], 0, 8);
 		
 	saveDataBuffer[0x141b] = 0;
 	saveDataBuffer[0x13bd] = 3;
 	saveDataBuffer[0x13b8] = 0xFF;
 
-	FSFILE_Write(saveDataHandle, 0, 0, saveDataBuffer, saveDataSize, FS_WRITE_FLUSH);
+	FSFILE_Write(saveDataHandle, &bytes, 0, saveDataBuffer, saveDataSize, FS_WRITE_FLUSH);
 		
 	free(saveDataBuffer);
 	FSFILE_Close(saveDataHandle);
@@ -65,21 +80,21 @@ u8 themeInstall()
 	FSUSER_DeleteFile(archive, fsMakePath(PATH_ASCII, "/BodyCache.bin"));
 	FSUSER_CreateFile(archive, fsMakePath(PATH_ASCII, "/BodyCache.bin"), 0, (u64)0x150000);
 	FSUSER_OpenFile(&bodyCache, archive, fsMakePath(PATH_ASCII, "/BodyCache.bin"), FS_OPEN_WRITE, 0);
-	FSFILE_Write(bodyCache, 0, 0, &body[0], (u64)bodySize, FS_WRITE_FLUSH);
+	FSFILE_Write(bodyCache, &bytes, 0, body, (u64)bodySize, FS_WRITE_FLUSH);
 	FSFILE_Close(bodyCache);
 
 	Handle bgmCache;
 	FSUSER_DeleteFile(archive, fsMakePath(PATH_ASCII, "/BgmCache.bin"));
 	FSUSER_CreateFile(archive, fsMakePath(PATH_ASCII, "/BgmCache.bin"), 0, (u64)3371008); 
 	FSUSER_OpenFile(&bgmCache, archive, fsMakePath(PATH_ASCII, "/BgmCache.bin"), FS_OPEN_WRITE, 0);
-	FSFILE_Write(bgmCache, 0, 0, &bgm[0], (u64)bgmSize, FS_WRITE_FLUSH);
+	FSFILE_Write(bgmCache, &bytes, 0, body, (u64)bgmSize, FS_WRITE_FLUSH);
 	FSFILE_Close(bgmCache);
 
 	u8* themeManageBuffer = malloc((sizeof(u8) * 2048) * 8);
 	Handle themeManageHandle;
 
 	FSUSER_OpenFile(&themeManageHandle, 0x000002cc, fsMakePath(PATH_ASCII, "/ThemeManage.bin"), FS_OPEN_WRITE, 0);
-	FSFILE_Read(themeManageHandle, 0, 0, themeManageBuffer, (u32)2048);
+	FSFILE_Read(themeManageHandle, &bytes, 0, themeManageBuffer, (u32)2048);
 
 	themeManageBuffer[0x00] = 1;
 	themeManageBuffer[0x01] = 0;
@@ -104,7 +119,7 @@ u8 themeInstall()
 	memset(&themeManageBuffer[0x360], 0, 4);
 	memset(&themeManageBuffer[0x368], 0, 4);
 
-	FSFILE_Write(themeManageHandle, 0, 0, themeManageBuffer, 2048, FS_WRITE_FLUSH);
+	FSFILE_Write(themeManageHandle, &bytes, 0, themeManageBuffer, 2048, FS_WRITE_FLUSH);
 	FSFILE_Close(themeManageHandle);
 
 	return 0;
@@ -115,12 +130,12 @@ int main(int argc, char **argv)
 {
 	gfxInitDefault();
 	cfguInit();
-
+	
 	consoleInit(GFX_TOP, NULL);
 
 	u8 ret = themeInstall();
 
-	printf("\x1b[29;15H%d", ret);
+	printf("\x1b[29;15H%u", ret);
 	
 	while (aptMainLoop())
 	{
