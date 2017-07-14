35b6b07477e763f90b95dff2aac5bbf4c5842b53
Dawid 'daedreth' Eckert <eckertdawid@gmail.com>
2017-07-14 13:26:24 +0200

    to live is to suffer

--- source/main.c
+++ source/main.c
@@ -16,11 +16,9 @@ int main(int argc, char **argv)
 
 	consoleInit(GFX_TOP, NULL);
 
-	s8 ret = prepareThemes();
-	s8 ret2 = themeInstall();
 
-	printf("\x1b[20;10H%d", ret);
-	printf("\x1b[29;15H%d", ret2);
+	printf("\x1b[20;10HN3DSThemeManager");
+
 	
 	while (aptMainLoop())
 	{
@@ -28,8 +26,8 @@ int main(int argc, char **argv)
 		u32 kDown = hidKeysDown();
 		if (kDown & KEY_START)
 		{
-				PTMSYSM_ShutdownAsync(0);
-					ptmSysmExit();
+			PTMSYSM_ShutdownAsync(0);
+			ptmSysmExit();
 		}
 		gfxFlushBuffers();
 		gfxSwapBuffers();
--- source/theme.c
+++ source/theme.c
@@ -54,18 +54,24 @@ s8 prepareThemes()
 
 s8 themeInstall()
 {
-	char* body = "";
-	char* bgm = "";
+	char *body;
+	char *bgm;
+	char *saveData;
+	char *themeManage;
 	u64 bodySize;
 	u64 bgmSize;
+	u64 saveDataSize;
+	u64 themeManageSize;
 	Handle bodyHandle;
 	Handle bgmHandle;
 	Handle saveDataHandle;
-	u8* saveDataBuffer;
-	u64 saveDataSize;
-	u32 bytes;
+	Handle themeManageHandle;
+
 	Result retValue;
 
+	u32 bytes;
+
+	// Opening relevant files
 	retValue = FSUSER_OpenFile(&bodyHandle, ArchiveSD, fsMakePath(PATH_ASCII, "/Themes/theme/body_LZ.bin"), FS_OPEN_READ, 0);
 	if(R_FAILED(retValue)) return R_SUMMARY(retValue);
 	retValue = FSFILE_GetSize(bodyHandle, &bodySize);
@@ -84,92 +90,88 @@ s8 themeInstall()
 	if(R_FAILED(retValue)) return R_SUMMARY(retValue);
 	FSFILE_Close(bgmHandle);
 
-	retValue = FSUSER_OpenFile(&saveDataHandle, ArchiveHomeExt, fsMakePath(PATH_ASCII, "/SaveData.dat"), FS_OPEN_READ | FS_OPEN_WRITE, 0);
+	retValue = FSUSER_OpenFile(&saveDataHandle, ArchiveHomeExt, fsMakePath(PATH_ASCII, "/SaveData.dat"), FS_OPEN_READ, 0);
 	if(R_FAILED(retValue)) return R_SUMMARY(retValue);
 	retValue = FSFILE_GetSize(saveDataHandle, &saveDataSize);
 	if(R_FAILED(retValue)) return R_SUMMARY(retValue);
-	saveDataBuffer = malloc(sizeof(u8) * saveDataSize);
-	retValue = FSFILE_Read(saveDataHandle, &bytes, 0, saveDataBuffer, (u32)saveDataSize);
+	saveData = malloc(sizeof(char) * saveDataSize);
+	retValue = FSFILE_Read(saveDataHandle, &bytes, 0, saveData, (u32)saveDataSize);
 	if(R_FAILED(retValue)) return R_SUMMARY(retValue);
-	retValue = memset(&saveDataBuffer[0x13b8], 0, 8);
-	if(R_FAILED(retValue)) return R_SUMMARY(retValue);
-
-	saveDataBuffer[0x141b] = 0x00;
-	saveDataBuffer[0x13b8] = 0xFF;
-	saveDataBuffer[0x13b9] = 0x00;
-	saveDataBuffer[0x13ba] = 0x00;
-	saveDataBuffer[0x13bb] = 0x00;
-	saveDataBuffer[0x13bc] = 0x00;
-	saveDataBuffer[0x13bd] = 0x03;
-	saveDataBuffer[0x13be] = 0x00;
-	saveDataBuffer[0x13bf] = 0x00;
-
-
-	retValue = FSFILE_Write(saveDataHandle, &bytes, 0, saveDataBuffer, saveDataSize, FS_WRITE_FLUSH);
-	if(R_FAILED(retValue)) return R_SUMMARY(retValue);
-	free(saveDataBuffer);
-	retValue = FSFILE_Close(saveDataHandle);
-	if(R_FAILED(retValue)) return R_SUMMARY(retValue);
-
-	Handle bodyCache;
-	retValue = FSUSER_DeleteFile(ArchiveThemeExt, fsMakePath(PATH_ASCII, "/BodyCache.bin"));
-	if(R_FAILED(retValue)) return R_SUMMARY(retValue);
-	retValue = FSUSER_CreateFile(ArchiveThemeExt, fsMakePath(PATH_ASCII, "/BodyCache.bin"), 0, (u64)0x150000);
-	if(R_FAILED(retValue)) return R_SUMMARY(retValue);
-	retValue = FSUSER_OpenFile(&bodyCache, ArchiveThemeExt, fsMakePath(PATH_ASCII, "/BodyCache.bin"), FS_OPEN_READ | FS_OPEN_WRITE, 0);
-	if(R_FAILED(retValue)) return R_SUMMARY(retValue);
-	retValue = FSFILE_Write(bodyCache, &bytes, 0, body, (u64)bodySize, FS_WRITE_FLUSH);
-	if(R_FAILED(retValue)) return R_SUMMARY(retValue);
-	FSFILE_Close(bodyCache);
-
-	Handle bgmCache;
-	retValue = FSUSER_DeleteFile(ArchiveThemeExt, fsMakePath(PATH_ASCII, "/BgmCache.bin"));
-	if(R_FAILED(retValue)) return R_SUMMARY(retValue);
-	retValue = FSUSER_CreateFile(ArchiveThemeExt, fsMakePath(PATH_ASCII, "/BgmCache.bin"), 0, (u64)3371008);
-	if(R_FAILED(retValue)) return R_SUMMARY(retValue);
-	retValue = FSUSER_OpenFile(&bgmCache, ArchiveThemeExt, fsMakePath(PATH_ASCII, "/BgmCache.bin"), FS_OPEN_READ | FS_OPEN_WRITE, 0);
-	if(R_FAILED(retValue)) return R_SUMMARY(retValue);
-	retValue = FSFILE_Write(bgmCache, &bytes, 0, bgm, (u64)bgmSize, FS_WRITE_FLUSH);
-	if(R_FAILED(retValue)) return R_SUMMARY(retValue);
-	FSFILE_Close(bgmCache);
-
-	u8* themeManageBuffer = malloc(sizeof(u8) * 2048);
-	Handle themeManageHandle;
-	u64 themeManageSize;
+	FSFILE_Close(saveDataHandle);	
+	
 	retValue = FSUSER_OpenFile(&themeManageHandle, ArchiveThemeExt, fsMakePath(PATH_ASCII, "/ThemeManage.bin"), FS_OPEN_READ | FS_OPEN_WRITE, 0);
 	if(R_FAILED(retValue)) return R_SUMMARY(retValue);
 	retValue = FSFILE_GetSize(themeManageHandle, &themeManageSize);
 	if(R_FAILED(retValue)) return R_SUMMARY(retValue);
-	retValue = FSFILE_Read(themeManageHandle, &bytes, 0, themeManageBuffer, (u32)themeManageSize);
-	if(R_FAILED(retValue)) return R_SUMMARY(retValue);
-
-	themeManageBuffer[0x00] = 1;
-	themeManageBuffer[0x01] = 0;
-	themeManageBuffer[0x02] = 0;
-	themeManageBuffer[0x03] = 0;
-	themeManageBuffer[0x04] = 0;
-	themeManageBuffer[0x05] = 0;
-	themeManageBuffer[0x06] = 0;
-	themeManageBuffer[0x07] = 0;
-
-	u32* bodySizeLocation = (u32*)(&themeManageBuffer[0x8]);
-	u32* bgmSizeLocation = (u32*)(&themeManageBuffer[0xC]);
-	*bodySizeLocation = (u32)bodySize;
-	*bgmSizeLocation = (u32)bgmSize;
-
-	themeManageBuffer[0x10] = 0xFF;
-	themeManageBuffer[0x14] = 0x01;
-	themeManageBuffer[0x18] = 0xFF;
-	themeManageBuffer[0x1D] = 0x02;
-
-	memset(&themeManageBuffer[0x338], 0, 4);
-	memset(&themeManageBuffer[0x340], 0, 4);
-	memset(&themeManageBuffer[0x360], 0, 4);
-	memset(&themeManageBuffer[0x368], 0, 4);
-
-	retValue = FSFILE_Write(themeManageHandle, &bytes, 0, themeManageBuffer, (u64)themeManageSize, FS_WRITE_FLUSH);
+	themeManage = malloc(sizeof(char) * themeManageSize);
+	retValue = FSFILE_Read(themeManageHandle, &bytes, 0, themeManage, (u32)themeManageSize);
 	if(R_FAILED(retValue)) return R_SUMMARY(retValue);
 	FSFILE_Close(themeManageHandle);
+
+	//Changing data in the buffers
+	memset(&saveData[0x13b8], 0, 8);
+
+	saveData[0x141b] = 0x00;
+	saveData[0x13b8] = 0xFF;
+	saveData[0x13b9] = 0x00;
+	saveData[0x13ba] = 0x00;
+	saveData[0x13bb] = 0x00;
+	saveData[0x13bc] = 0x00;
+	saveData[0x13bd] = 0x03;
+	saveData[0x13be] = 0x00;
+	saveData[0x13bf] = 0x00;
+
+
+       	themeManage[0x00] = 1;
+       	themeManage[0x01] = 0;
+       	themeManage[0x02] = 0;
+	themeManage[0x03] = 0;
+        themeManage[0x04] = 0;
+        themeManage[0x05] = 0;
+        themeManage[0x06] = 0;
+        themeManage[0x07] = 0;
+
+        u32* bodySizeLocation = (u32*)(&themeManage[0x8]);
+        u32* bgmSizeLocation = (u32*)(&themeManage[0xC]);
+        *bodySizeLocation = (u32)bodySize;
+        *bgmSizeLocation = (u32)bgmSize;
+
+        themeManage[0x10] = 0xFF;
+        themeManage[0x14] = 0x01;
+        themeManage[0x18] = 0xFF;
+        themeManage[0x1D] = 0x02;
+
+        memset(&themeManage[0x338], 0, 4);
+        memset(&themeManage[0x340], 0, 4);
+        memset(&themeManage[0x360], 0, 4);
+        memset(&themeManage[0x368], 0, 4);
+
+
+	// Writing to extdata
+	retValue = FSUSER_OpenFile(&bodyHandle, ArchiveThemeExt, fsMakePath(PATH_ASCII, "/BodyCache.bin"), FS_OPEN_READ | FS_OPEN_WRITE, 0);
+	if(R_FAILED(retValue)) return R_SUMMARY(retValue);
+	retValue = FSFILE_Write(bodyHandle, &bytes, 0, body, (u64)bodySize, FS_WRITE_FLUSH);
+	if(R_FAILED(retValue)) return R_SUMMARY(retValue);
+	FSFILE_Close(bodyHandle);
+
+	retValue = FSUSER_OpenFile(&bgmHandle, ArchiveThemeExt, fsMakePath(PATH_ASCII, "/BgmCache.bin"), FS_OPEN_READ | FS_OPEN_WRITE, 0);
+	if(R_FAILED(retValue)) return R_SUMMARY(retValue);
+	retValue = FSFILE_Write(bgmHandle, &bytes, 0, bgm, (u64)bgmSize, FS_WRITE_FLUSH);
+	if(R_FAILED(retValue)) return R_SUMMARY(retValue);
+	FSFILE_Close(bgmHandle);
+	
+	retValue = FSUSER_OpenFile(&themeManageHandle, ArchiveThemeExt, fsMakePath(PATH_ASCII, "/ThemeManage.bin"), FS_OPEN_READ | FS_OPEN_WRITE, 0);
+	if(R_FAILED(retValue)) return R_SUMMARY(retValue);
+	retValue = FSFILE_Write(themeManageHandle, &bytes, 0, themeManage, (u64)themeManageSize, FS_WRITE_FLUSH);
+	if(R_FAILED(retValue)) return R_SUMMARY(retValue);
+	FSFILE_Close(themeManageHandle);
+
+	retValue = FSUSER_OpenFile(&saveDataHandle, ArchiveHomeExt, fsMakePath(PATH_ASCII, "/SaveData.dat"), FS_OPEN_READ | FS_OPEN_WRITE, 0);
+	if(R_FAILED(retValue)) return R_SUMMARY(retValue);
+	retValue = FSFILE_Write(saveDataHandle, &bytes, 0, saveData, (u64)saveDataSize, FS_WRITE_FLUSH);
+	if(R_FAILED(retValue)) return R_SUMMARY(retValue);
+	FSFILE_Close(saveDataHandle);
+
 
 	FSUSER_CloseArchive(ArchiveSD);
 	FSUSER_CloseArchive(ArchiveHomeExt);
