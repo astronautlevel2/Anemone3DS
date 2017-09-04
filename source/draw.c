a38508c30fe629097ebca92474a2d46006ef8bfc
Helloman892 <1565516+Helloman892@users.noreply.github.com>
2017-09-04 22:43:42 +0100

    undoes part of commit d3a8f79

--- source/draw.c
+++ source/draw.c
@@ -115,11 +115,6 @@ void draw_qr(void)
     }
 }
 
-void draw_shuffle_selection(int shuffled_theme_count)
-{
-    pp2d_draw_textf(36, 2, 0.6, 0.6, COLOR_WHITE, "%i/10", shuffled_theme_count);
-}
-
 void draw_base_interface(void)
 {
     pp2d_begin_draw(GFX_TOP);
