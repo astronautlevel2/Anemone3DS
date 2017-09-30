53961f269d56ace650002cdb0018b8d4f27fbbd7
Dylan G <1565516+Helloman892@users.noreply.github.com>
2017-09-30 03:19:54 +0100

    patch 1.2.2 finishing commits (#91)
    * Fixing a bad fix of mine '_>'
    
    * Getting ever closer to fixing the SMDH bug
    
    * Make sure that the buffer text is copied into is properly null-terminated

--- include/themes.h
+++ include/themes.h
@@ -30,9 +30,9 @@
 #include "common.h"
 
 typedef struct {
-    u16 name[0x40];
-    u16 desc[0x80];
-    u16 author[0x40];
+    u16 name[0x41];
+    u16 desc[0x81];
+    u16 author[0x41];
     
     u32 placeholder_color;
     ssize_t icon_id;
