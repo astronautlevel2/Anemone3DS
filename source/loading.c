a79869f396e2e2ac9a688f05c24788bd04993629
LiquidFenrir <liquidfenrir@outlook.fr>
2018-05-18 19:14:22 +0200

    support for more types of png files thanks a ton to https://gist.github.com/niw/5963798

--- source/loading.c
+++ source/loading.c
@@ -444,9 +444,40 @@ bool load_preview_from_buffer(void * buf, u32 size, C2D_Image * preview_image, i
     int width = png_get_image_width(png, info);
     int height = png_get_image_height(png, info);
 
+    png_byte color_type = png_get_color_type(png, info);
+    png_byte bit_depth  = png_get_bit_depth(png, info);
+
+    // Read any color_type into 8bit depth, ABGR format.
+    // See http://www.libpng.org/pub/png/libpng-manual.txt
+
+    if(bit_depth == 16)
+        png_set_strip_16(png);
+
+    if(color_type == PNG_COLOR_TYPE_PALETTE)
+        png_set_palette_to_rgb(png);
+
+    // PNG_COLOR_TYPE_GRAY_ALPHA is always 8 or 16bit depth.
+    if(color_type == PNG_COLOR_TYPE_GRAY && bit_depth < 8)
+        png_set_expand_gray_1_2_4_to_8(png);
+
+    if(png_get_valid(png, info, PNG_INFO_tRNS))
+        png_set_tRNS_to_alpha(png);
+
+    // These color_type don't have an alpha channel then fill it with 0xff.
+    if(color_type == PNG_COLOR_TYPE_RGB ||
+       color_type == PNG_COLOR_TYPE_GRAY ||
+       color_type == PNG_COLOR_TYPE_PALETTE)
+        png_set_filler(png, 0xFF, PNG_FILLER_AFTER);
+
+    if(color_type == PNG_COLOR_TYPE_GRAY ||
+       color_type == PNG_COLOR_TYPE_GRAY_ALPHA)
+        png_set_gray_to_rgb(png);
+
     //output ABGR
     png_set_bgr(png);
     png_set_swap_alpha(png);
+
+    png_read_update_info(png, info);
 
     png_bytep * row_pointers = (png_bytep*)malloc(sizeof(png_bytep) * height);
     for(int y = 0; y < height; y++) {
