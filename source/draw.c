838cf14f439333e77ead042f30aa8c634e3f57ed
Dylan G <1565516+Helloman892@users.noreply.github.com>
2018-05-19 23:24:40 +0100

    Add draw_text_wrap (#169)

--- source/draw.c
+++ source/draw.c
@@ -374,6 +374,52 @@ static void draw_instructions(Instructions_s instructions)
     */
 }
 
+// implementation detail - if max_width <= 0, does nothing (for sanity)
+static void draw_text_wrap(float x, float y, float z, float scaleX, float scaleY, Color color, const char * text, float max_width)
+{
+    if (max_width <= 0)
+        return;
+
+    int length = strlen(text) + 1;
+    char result[length];
+    memset(result, 0, length);
+    int idx = 0;
+
+    float current_width = 0;
+
+    while (*text)
+    {
+        ssize_t consumed;
+        u32 codepoint;
+        
+        if ((consumed = decode_utf8(&codepoint, (unsigned char*)text)) == -1)
+            break;
+        float character_width = scaleX * (fontGetCharWidthInfo(fontGlyphIndexFromCodePoint(codepoint))->charWidth);
+        if ((current_width += character_width) > max_width)
+        {
+            char* last_space = NULL;
+            for (int i = idx; i >= 0; i--)
+            {
+                if (result[i] == ' ')
+                {
+                    last_space = &result[i];
+                    break;
+                }
+            }
+            if (last_space != NULL)
+                *last_space = '\n';
+            else
+                result[idx++] = '\n';
+            current_width = 0;
+        }
+        memcpy(&result[idx], text, consumed);
+        idx += consumed;
+        text += consumed;
+    }
+
+    draw_text(x, y, z, scaleX, scaleY, color, result);
+}
+
 static void draw_entry_info(Entry_s * entry)
 {
     char author[0x41] = {0};
@@ -389,7 +435,7 @@ static void draw_entry_info(Entry_s * entry)
 
     char description[0x81] = {0};
     utf16_to_utf8((u8*)description, entry->desc, 0x80);
-    // draw_text_wrap(20, 363, 50+count*height, 0.5, 0.5, 0.5, colors[COLOR_WHITE], description);
+    draw_text_wrap(20, 70, 0.5, 0.5, 0.5, colors[COLOR_WHITE], description, 363);
 }
 
 void draw_grid_interface(Entry_List_s* list, Instructions_s instructions)
