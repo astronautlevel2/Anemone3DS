08ff0196122f4f139d5b2549c6178cfb43e44d11
LiquidFenrir <liquidfenrir@outlook.fr>
2019-01-08 23:13:07 +0100

    add badge preview

--- source/preview.cpp
+++ source/preview.cpp
@@ -165,10 +165,108 @@ void PreviewImage::draw() const
 
 BadgePreviewImage::BadgePreviewImage(const fs::path& path)
 {
+    FILE* fh = fopen(path.c_str(), "rb");
 
+    u8 sig[8];
+    fread(sig, 1, 8, fh);
+    if(png_sig_cmp(sig, 0, 8))
+    {
+        fclose(fh);
+        return;
+    }
+
+    fseek(fh, 0, SEEK_SET);
+
+    png_structp png = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+    png_infop info = png_create_info_struct(png);
+
+    png_init_io(png, fh);
+    png_read_info(png, info);
+
+    int width = png_get_image_width(png, info);
+    int height = png_get_image_height(png, info);
+
+    png_byte color_type = png_get_color_type(png, info);
+    png_byte bit_depth  = png_get_bit_depth(png, info);
+
+    // Read any color_type into 8bit depth, ABGR format.
+    // See http://www.libpng.org/pub/png/libpng-manual.txt
+
+    if(bit_depth == 16)
+        png_set_strip_16(png);
+
+    if(color_type == PNG_COLOR_TYPE_PALETTE)
+        png_set_palette_to_rgb(png);
+
+    // PNG_COLOR_TYPE_GRAY_ALPHA is always 8 or 16bit depth.
+    if(color_type == PNG_COLOR_TYPE_GRAY && bit_depth < 8)
+        png_set_expand_gray_1_2_4_to_8(png);
+
+    if(png_get_valid(png, info, PNG_INFO_tRNS))
+        png_set_tRNS_to_alpha(png);
+
+    // These color_type don't have an alpha channel then fill it with 0xff.
+    if(color_type == PNG_COLOR_TYPE_RGB ||
+       color_type == PNG_COLOR_TYPE_GRAY ||
+       color_type == PNG_COLOR_TYPE_PALETTE)
+        png_set_filler(png, 0xFF, PNG_FILLER_AFTER);
+
+    if(color_type == PNG_COLOR_TYPE_GRAY ||
+       color_type == PNG_COLOR_TYPE_GRAY_ALPHA)
+        png_set_gray_to_rgb(png);
+
+    //output ABGR
+    png_set_bgr(png);
+    png_set_swap_alpha(png);
+
+    png_read_update_info(png, info);
+
+    png_bytep* row_pointers = new(std::nothrow) png_bytep[height];
+    for(int y = 0; y < height; y++)
+        row_pointers[y] = new(std::nothrow) png_byte[png_get_rowbytes(png,info)];
+
+    png_read_image(png, row_pointers);
+
+    png_destroy_read_struct(&png, &info, NULL);
+    fclose(fh);
+
+    u16 w_nextPow2 = nextPow2(width);
+    u16 h_nextPow2 = nextPow2(height);
+
+    Tex3DS_SubTexture* subt3x = new(std::nothrow) Tex3DS_SubTexture;
+    subt3x->width = width;
+    subt3x->height = height;
+    subt3x->left = 0.0f;
+    subt3x->top = 1.0f;
+    subt3x->right = static_cast<float>(width)/w_nextPow2;
+    subt3x->bottom = 1.0-(static_cast<float>(height)/h_nextPow2);
+    this->image->subtex = subt3x;
+
+    C3D_TexInit(this->image->tex, w_nextPow2, h_nextPow2, GPU_RGBA8);
+
+    for(int j = 0; j < height; j++)
+    {
+        png_bytep row = row_pointers[j];
+        for(int i = 0; i < width; i++)
+        {
+            png_bytep px = &(row[i * sizeof(u32)]);
+            u32 dst = ((((j >> 3) * (w_nextPow2 >> 3) + (i >> 3)) << 6) + ((i & 1) | ((j & 1) << 1) | ((i & 2) << 1) | ((j & 2) << 2) | ((i & 4) << 2) | ((j & 4) << 3))) * 4;
+
+            memcpy(static_cast<u8*>(this->image->tex->data) + dst, px, sizeof(u32));
+        }
+        delete[] row;
+    }
+    delete[] row_pointers;
+
+    this->ready = true;
 }
 
 void BadgePreviewImage::draw() const
 {
-
+    start_frame(COLOR_BLACK);
+    switch_screen(GFX_TOP);
+    C2D_DrawImageAt(*(this->image), 0.0f, 0.0f, 0.0f);
+    switch_screen(GFX_BOTTOM);
+    C2D_DrawImageAt(*(this->image), 0.0f, -240.0f, 0.0f);
+    end_frame();
 }
