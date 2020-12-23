astronautlevel2/Anemone3DS

commit 1c2e562dd60348174fae25d6714d026b68fa3da0

Networking rework (#240)
* Began refactoring http_get. Still a few cases to handle, but it works.

* Introduced error enum; handle error 406; handle a misbehaving server; redirect_url leaked memory (L888)

* Allowed calls to http_get to pass NULL as acceptable_mime_types

* Removed status_code from header struct; eventually planning to move most of parse_header's logic back to http_get, so this makes sense

* Moved some more logic back into http_get
Note: currently behaves weirdly with some QR codes, including but probably not limited to ones pointing to TinyURL.

* Handle redirects correctly; other rearrangements

* Formatting

* Handle HTTP 303 See Other correctly

* Removed "Download failed" error in camera.c due to all related failures being handled in http_get

* Fixed missing loading bar; started working on unchunked download

* Added unchunked download

* Reintroduced the download progress bar, rolled back to always doing chunked download

* URL length caps at 2083 chars

* Slightly more efficient

* Correctly handle incorrect filesize header
TODO: reorder clauses to remove the goto?

* Fixed illegal characters, removed unnecessary logic (technically a regression, as we always have to realloc now)

* Finally finished up status code handling. Fixed a memory leak when the server fails conneg.

source/camera.c
@@ -229,7 +229,7 @@ void update_qr(qr_data *data)
             draw_install(INSTALL_DOWNLOAD);
             char * zip_buf = NULL;
             char * filename = NULL;
-            u32 zip_size = http_get((char*)scan_data.payload, &filename, &zip_buf, INSTALL_DOWNLOAD);
+            u32 zip_size = http_get((char*)scan_data.payload, &filename, &zip_buf, INSTALL_DOWNLOAD, "application/zip");
 
             if(zip_size != 0)
             {
@@ -288,18 +288,16 @@ void update_qr(qr_data *data)
                     }
                     else
                     {
+                        // TODO: handle more elegantly
                         throw_error("Zip downloaded is neither\na splash nor a theme.", ERROR_LEVEL_WARNING);
                     }
                 }
                 else
                 {
+                    // TODO: marked for deletion
                     throw_error("File downloaded isn't a zip.", ERROR_LEVEL_WARNING);
                 }
                 free(zip_buf);
-            }
-            else
-            {
-                throw_error("Download failed.", ERROR_LEVEL_WARNING);
             }
 
             free(filename);

include/remote.h
@@ -52,6 +52,6 @@
 #define CACHE_PATH_FORMAT            "/3ds/"  APP_TITLE  "/cache/%"  JSON_INTEGER_FORMAT
 
 bool themeplaza_browser(EntryMode mode);
-u32 http_get(const char *url, char ** filename, char ** buf, InstallType install_type);
+u32 http_get(const char *url, char ** filename, char ** buf, InstallType install_type, const char *mime_type);
 
 #endif

commit a2b6eb94e4a2f93fc0be2b5450dc8536b42be4ac

Fix regressions introduced in 1c2e562dd60348174fae25d6714d026b68fa3da0

source/camera.c
@@ -332,7 +332,7 @@ bool init_qr(void)
         draw_install(INSTALL_DOWNLOAD);
         char * zip_buf = NULL;
         char * filename = NULL;
-        u32 zip_size = http_get((char*)scan_data->payload, &filename, &zip_buf, INSTALL_DOWNLOAD);
+        u32 zip_size = http_get((char*)scan_data->payload, &filename, &zip_buf, INSTALL_DOWNLOAD, "application/zip");
 
         if(zip_size != 0)
         {

source/remote.c
@@ -892,6 +892,7 @@ redirect: // goto here if we need to redirect
     ParseResult parse = parse_header(&_header, &context, (bool)filename, acceptable_mime_types);
     switch (parse)
     {
+    char err_buf[0x69];
     case NO_FILENAME:
     case SUCCESS:
         break;
@@ -926,11 +927,10 @@ redirect: // goto here if we need to redirect
         quit = true;
         return httpcCloseContext(&context);
     case HTTP_NOT_FOUND:
-    case HTTP_GONE:
+    case HTTP_GONE: ;
         // TODO: check if we're looking at a TP URL, if we are, suggest that it might be missing
         const char * http_error = parse == HTTP_NOT_FOUND ? "404 Not Found" : "410 Gone";
         DEBUG("HTTP %s; URL: %s\n", http_error, url);
-        char err_buf[0x69];
         snprintf(err_buf, 0x69, "HTTP %s\nCheck that the URL is correct.", http_error);
         throw_error(err_buf, ERROR_LEVEL_WARNING);
         return httpcCloseContext(&context);
@@ -938,7 +938,6 @@ redirect: // goto here if we need to redirect
     case HTTP_FORBIDDEN:
     case HTTP_PROXY_UNAUTHORIZED:
         DEBUG("HTTP %u: device not authenticated\n", parse);
-        char err_buf[0x69];
         snprintf(err_buf, 0x69, "HTTP %s\nContact the site administrator.", parse == HTTP_UNAUTHORIZED
             ? "401 Unauthorized"
             : parse == HTTP_FORBIDDEN
@@ -979,13 +978,10 @@ redirect: // goto here if we need to redirect
         throw_error("HTTP 504 Gateway Timeout\nContact the site administrator.", ERROR_LEVEL_WARNING);
         return httpcCloseContext(&context);
     default:
-    {
         DEBUG("HTTP %u\n", parse);
-        char err_buf[0x69];
         snprintf(err_buf, 0x69, "HTTP %u\nIf you believe this is unexpected, please\ncontact the site administrator.", parse);
         throw_error(err_buf, ERROR_LEVEL_WARNING);
         return httpcCloseContext(&context);
-    }
     }
 
     if (filename)