7c027bad05858e8644ab3e74053b4121949e25fb
cooolgamer <64099608+cooolgamer@users.noreply.github.com>
2024-06-14 19:04:22 +0200

    add Korean translations placeholder to fit the text (because big font) + added message for Korean patch

--- source/ui_strings.c
+++ source/ui_strings.c
@@ -1167,7 +1167,7 @@ const Language_s language_french = {
             .instructions = {
                 {
                     "\uE079 Aller à la page",
-                    "\uE07A Tags de recherche"
+                    "\uE07A Recherche par tag"
                 },
                 {
                     NULL,
@@ -1652,6 +1652,415 @@ const Language_s language_portuguese = {
     }
 };
 
+const Language_s language_korean = {
+    // As you can see, this isn't korean. This is a placeholder so that the text is visible correctly because the Korean font is big.
+    // Translations are welcomed!
+    .normal_instructions = 
+    {
+        {
+            .info_line = NULL,
+            .instructions = {
+                {
+                    "\uE000 Install Theme(s)",
+                    "\uE001 Queue shuffle"
+                },
+                {
+                    "\uE002 More options",
+                    "\uE003 Preview theme"
+                },
+                {
+                    "\uE004 Go to splashes",
+                    "\uE005 Scan QR code"
+                },
+                {
+                    "Exit",
+                    "Delete from SD"
+                }
+            }
+        },
+        {
+            .info_line = NULL,
+            .instructions = {
+                {
+                    "\uE000 Install splash",
+                    "\uE001 Delete cur. splash"
+                },
+                {
+                    "\uE002 More options",
+                    "\uE003 Preview splash"
+                },
+                {
+                    "\uE004 Go to themes",
+                    "\uE005 Scan QR code"
+                },
+                {
+                    "Exit",
+                    "Delete from SD"
+                }
+            }
+        }
+    },
+
+    .install_instructions =
+    {
+        .info_line = "\uE001 Cancel theme install",
+        .instructions = {
+            {
+                "\uE079 Normal install",
+                "\uE07A Shuffle install"
+            },
+            {
+                "\uE07B BGM-only install",
+                "\uE07C No-BGM install"
+            },
+            {
+                NULL,
+                NULL
+            },
+            {
+                "Exit",
+                NULL
+            }
+        }
+    },
+
+    .extra_instructions = 
+    {
+        {
+            .info_line = "\uE001 Leave sorting menu",
+            .instructions = {
+                {
+                    "\uE079 Sort by name",
+                    "\uE07A Sort by author"
+                },
+                {
+                    "\uE07B Sort by filename",
+                    NULL
+                },
+                {
+                    NULL,
+                    NULL
+                },
+                {
+                    "Exit",
+                    NULL
+                }
+            }
+        },
+        {
+            .info_line = "\uE001 Leave extra menu",
+            .instructions = {
+                {
+                    "\uE079 Jump in the list",
+                    "\uE07A Reload icons"
+                },
+                {
+                    "\uE07B Browse TP",
+                    "\uE07C Install Badges"
+                },
+                {
+                    "\uE004 Sorting menu",
+                    "\uE005 Dump..."
+                },
+                {
+                    "Exit",
+                    NULL
+                }
+            }
+        },
+        {
+            .info_line = "Dump... | \uE001 Leave dump menu",
+            .instructions = {
+                {
+                    "\uE079 Installed Theme",
+                    "\uE07A All Themes"
+                },
+                {
+                    "\uE07B Badges",
+                    NULL
+                },
+                {
+                    NULL,
+                    NULL
+                },
+                {
+                    "Exit",
+                    NULL
+                }
+            }
+        }
+    },
+    .camera = 
+    {
+        .quit = "Press \uE005 To Quit",
+        .thread_error = "Capture cam thread creation failed\nPlease report this to the developers",
+        .zip_not_theme_splash = "Zip downloaded is neither\na splash nor a theme",
+        .file_not_zip = "File downloaded isn't a zip.",
+        .download_failed = "Download failed.",
+        .badge_question = "Theme or splash not detected.\nIs this a badge?",
+    },
+    .draw = 
+    {
+        .theme_mode = "Theme mode",
+        .splash_mode = "Splash mode",
+        .no_themes = "No theme found",
+        .no_splashes = "No splash found",
+        .qr_download = "Press \uE005 to dl from QR",
+        .switch_splashes = "Or \uE004 to go to splashes",
+        .switch_themes = "Or \uE004 to go to themes",
+        .quit = "Or        to quit",
+        .start_pos = 162, // Adjust x pos of start glyph to line up with quit string
+        .by = "By ",
+        .selected = "Selected:",
+        .sel = "Sel.:",
+        .tp_theme_mode = "ThemePlaza Theme mode",
+        .tp_splash_mode = "ThemePlaza Splash mode",
+        .tp_badge_mode = "ThemePlaza Badge mode",
+        .search = "Search...",
+        .page = "Page:",
+        .err_quit = "Press \uE000 to quit.",
+        .warn_continue = "Press \uE000 to continue.",
+        .yes_no = "\uE000 Yes   \uE001 No",
+        .load_themes = "Loading themes, please wait...",
+        .load_splash = "Loading splashes, please wait...",
+        .load_icons = "Loading icons, please wait...",
+        .install_splash = "Installing a splash...",
+        .delete_splash = "Deleting installed splash...",
+        .install_theme = "Installing a single theme...",
+        .install_shuffle = "Installing shuffle themes...",
+        .install_bgm = "Installing BGM-only theme...",
+        .install_no_bgm = "Installing theme w/o BGM...",
+        .downloading = "Downloading...",
+        .checking_dl = "Checking downloaded file...",
+        .delete_sd = "Deleting from SD...",
+        .download_themes = "Downloading theme list,\nplease wait...",
+        .download_splashes = "Downloading splash list,\nplease wait...",
+        .download_badges = "Downloading badge list,\nplease wait...",
+        .download_preview = "Downloading preview,\nplease wait...",
+        .download_bgm = "Downloading BGM, please wait...",
+        .dump_single = "Dumping theme, please wait...",
+        .dump_all_official = "Dumping official themes,\nplease wait...",
+        .dump_badges = "Dumping installed badges,\nplease wait...",
+        .install_badges = "Installing badges, please wait...",
+        .shuffle = "Shuffle: %i/10",
+    },
+    .fs =
+    {
+        .illegal_input = "Input must not contain:\n" ILLEGAL_CHARS,
+        .new_or_overwrite = "Choose a new filename or tap Overwrite",
+        .cancel = "Cancel",
+        .overwrite = "Overwrite",
+        .rename = "Rename",
+        .swkbd_fail = "???\nTry a USB keyboard", // Should never be used
+        .sd_full = "SD card is full.\nDelete some themes to make space.",
+        .fs_error = "Error:\nGet a new SD card.",
+    },
+    .loading =
+    {
+        .no_preview = "No preview found.",
+    },
+    .main =
+    {
+        .position_too_big = "The new position has to be\nsmaller or equal to the\nnumber of entries!",
+        .position_zero = "The new position has to\nbe positive!",
+        .jump_q = "Where do you want to jump to?\nMay cause icons to reload.",
+        .cancel = "Cancel",
+        .jump = "Jump",
+        // Note to translator: This is a special case, please translate this string instead of the original one.
+        .no_theme_extdata = "Theme extdata does not exist!\nPlease install the Korean patch\nthen set a default theme\nfrom the homemenu settings.",
+        .loading_qr = "Loading QR Scanner...",
+        .no_wifi = "Please connect to Wi-Fi\nbefore scanning QR codes",
+        .qr_homebrew = "QR scanning doesnt work from the Homebrew\nLauncher, use the ThemePlaza browser instead.",
+        .camera_broke = "Your camera seems to have a problem,\nunable to scan QR codes.",
+        .too_many_themes = "You have too many themes selected.",
+        .not_enough_themes = "You don't have enough themes selected.",
+        .uninstall_confirm = "Are you sure you would like to delete\nthe installed splash?",
+        .delete_confirm = "Are you sure you would like\nto delete this?",
+    },
+    .remote =
+    {
+        .no_results = "No results for this search.",
+        .check_wifi = "Couldn't download Theme Plaza data.\nMake sure WiFi is on.",
+        .new_page_big = "The new page has to be\nsmaller or equal to the\nnumber of pages!",
+        .new_page_zero = "The new position has to\nbe positive!",
+        .jump_page = "Which page do you want to jump to?",
+        .cancel = "Cancel",
+        .jump = "Jump",
+        .tags = "Which tags do you want to search for?",
+        .search = "Search",
+        .parental_fail = "Parental Control validation failed!\nBrowser Access restricted.",
+        .zip_not_found = "ZIP not found at this URL\nIf you believe this is an error, please\ncontact the site administrator",
+        .generic_httpc_error = "Error in HTTPC sysmodule - 0x%08lx.\nIf you are seeing this, please contact an\nAnemone developer on the Theme Plaza Discord.",
+        .http303_tp = "HTTP 303 See Other (Theme Plaza)\nHas this theme been approved?",
+        .http303 = "HTTP 303 See Other\nDownload the resource directly\nor contact the site administrator.",
+        .http404 = "HTTP 404 Not Found\nHas this theme been approved?",
+        .http_err_url = "HTTP %s\nCheck that the URL is correct.",
+        .http_errcode_generic = "HTTP %s\nContact the site administrator.",
+        .http401 = "401 Unauthorized",
+        .http403 = "403 Forbidden",
+        .http407 = "407 Proxy Authentication Required",
+        .http414 = "HTTP 414 URI Too Long\nThe QR code points to a really long URL.\nDownload the file directly.",
+        .http418 = "HTTP 418 I'm a teapot\nContact the site administrator.",
+        .http426 = "HTTP 426 Upgrade Required\nThe 3DS cannot connect to this server.\nContact the site administrator.",
+        .http451 = "HTTP 451 Unavailable for Legal Reasons\nSome entity is preventing access\nto the host server for legal reasons.",
+        .http500 = "HTTP 500 Internal Server Error\nContact the site administrator.",
+        .http502 = "HTTP 502 Bad Gateway\nContact the site administrator.",
+        .http503 = "HTTP 503 Service Unavailable\nContact the site administrator.",
+        .http504 = "HTTP 504 Gateway Timeout\nContact the site administrator.",
+        .http_unexpected = "HTTP %u\nIf you believe this is unexpected, please\ncontact the site administrator.",
+    },
+    .remote_instructions =
+    {
+        {
+            .info_line = NULL,
+            .instructions = {
+                {
+                    "\uE000 Download theme",
+                    "\uE001 Go back"
+                },
+                {
+                    "\uE002 More options",
+                    "\uE003 Preview theme"
+                },
+                {
+                    "\uE004 Previous page",
+                    "\uE005 Next page"
+                },
+                {
+                    "Exit",
+                    NULL
+                }
+            }
+        },
+        {
+            .info_line = NULL,
+            .instructions = {
+                {
+                    "\uE000 Download splash",
+                    "\uE001 Go back"
+                },
+                {
+                    "\uE002 More options",
+                    "\uE003 Preview splash"
+                },
+                {
+                    "\uE004 Previous page",
+                    "\uE005 Next page"
+                },
+                {
+                    "Exit",
+                    NULL
+                }
+            }
+        },
+        {
+            .info_line = NULL,
+            .instructions = {
+                {
+                    "\uE000 Download badges",
+                    "\uE001 Go back"
+                },
+                {
+                    "\uE002 More options",
+                    "\uE003 Preview badges"
+                },
+                {
+                    "\uE004 Previous page",
+                    "\uE005 Next page"
+                },
+                {
+                    "Exit",
+                    NULL
+                }
+            }
+        }
+    },
+    .remote_extra_instructions =
+    {
+        {
+            .info_line = "\uE001 Leave extra menu",
+            .instructions = {
+                {
+                    "\uE079 Jump to page",
+                    "\uE07A Search tags"
+                },
+                {
+                    NULL,
+                    "\uE07C Reload w/o cache"
+                },
+                {
+                    "\uE004 Go to Badges",
+                    "\uE005 Go to Splashes"
+                },
+                {
+                    "Exit",
+                    NULL
+                }
+            }
+        },
+        {
+            .info_line = "\uE001 Leave extra menu",
+            .instructions = {
+                {
+                    "\uE079 Jump to page",
+                    "\uE07A Search tags"
+                },
+                {
+                    NULL,
+                    "\uE07C Reload w/o cache"
+                },
+                {
+                    "\uE004 Go to Themes",
+                    "\uE005 Go to Badges"
+                },
+                {
+                    "Exit",
+                    NULL
+                }
+            }
+        },
+        {
+            .info_line = "\uE001 Leave extra menu",
+            .instructions = {
+                {
+                    "\uE079 Jump to page",
+                    "\uE07A Search tags"
+                },
+                {
+                    NULL,
+                    "\uE07C Reload w/o cache"
+                },
+                {
+                    "\uE004 Go to Splashes",
+                    "\uE005 Go to Themes"
+                },
+                {
+                    "Exit",
+                    NULL
+                }
+            }
+        }
+    },
+    .splashes =
+    {
+        .no_splash_found = "No splash.bin or splashbottom.bin found.\nIs this a splash?",
+        .splash_disabled = "WARNING: Splashes are disabled in Luma Config",
+    },
+    .themes =
+    {
+        .no_body_found = "No body_LZ.bin found - is this a theme?",
+        .mono_warn = "One or more installed themes\nuse mono audio.\nMono audio causes a number of issues.\nCheck the wiki for more information.",
+        .illegal_char = "Illegal character used.",
+        .name_folder = "Name of output folder",
+        .cancel = "Cancel",
+        .done = "Done"
+    },
+    .badges = 
+    {
+        .extdata_locked = "Ext Data Locked\nTry pressing the Home Button\nand then returning to Anemone3DS,\nor using the CIA version instead."
+    }
+};
+
 Language_s init_strings(CFG_Language lang)
 {
     switch (lang)
@@ -1664,7 +2073,8 @@ Language_s init_strings(CFG_Language lang)
         case CFG_LANGUAGE_ES:
             return language_spanish;
         //case CFG_LANGUAGE_ZH:
-        //case CFG_LANGUAGE_KO:
+        case CFG_LANGUAGE_KO:
+            return language_korean;
         //case CFG_LANGUAGE_NL:
         case CFG_LANGUAGE_PT:
             return language_portuguese;
